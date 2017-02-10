2ea2c9393980de5fb0502c24e0e0f9e0f2b22df7
ryo-murai <murai.ry@gmail.com>
2017-02-10 18:11:53 +0900

    initial commit, files generated by spring initializr

---
+++ src/main/java/io/github/murtools/mithai/MithaiApplication.java
@@ -0,0 +1,12 @@
+package io.github.murtools.mithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MithaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MithaiApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/murtools/mithai/MithaiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.murtools.mithai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MithaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
